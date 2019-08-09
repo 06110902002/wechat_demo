a9895c5690b8c93d56a597d9586bb075293dc05a
Liu Xiaobing <liuxiaobing@G114E1901117.local>
2019-08-09 17:35:34 +0800

    处理推荐内容列表

--- pages/main/main.js
+++ pages/main/main.js
@@ -43,7 +43,38 @@ Page({
 
 
     currentSwiper: 0,
-    autoplay: true
+    autoplay: true,
+
+    //点赞评论数组
+    like_comments:[
+      {
+        headImg:'./img/menu1.png',nick:'韦小宝',lasttime:'20分钟前',
+        title:'#小七与咩咩来约会',
+        content:'adsadasdfasdfasdfasdfasdfasdf',
+        imgs: ['./img/menu1.png', './img/menu2.png', './img/menu3.png', './img/menu5.png'],
+        comments:'23',
+        like:'12'
+      },
+
+      {
+        headImg: './img/menu6.png', nick: '乔峰', lasttime: '20分钟前',
+        title: '#小七与咩咩来约会',
+        content: '马路上此起彼伏的车鸣声，夹杂着凄凄惨惨的雨滴，倒也洗去了城市街头的龌蹉与浊尘。我无力地将头靠在起雾的车窗一角，无神的眼 眸，望向远方，默默承受着会呼吸的伤和痛.在失去点点后，我曾一度伤                   心好几周，每当看见它熟悉的玩偶和那独有的小窝时，心就被揪似的疼。不知为何，我突然萌生出再也不想养狗的想法，是的，我很爱狗狗，但是，我更怕这种一辈子也无法抹去的伤痕',
+        imgs: ['./img/menu1.png', './img/menu2.png', './img/menu3.png'],
+        comments: '23',
+        like: '12'
+      },
+
+      {
+        headImg: './img/menu7.png', nick: '刘胡来', lasttime: '20分钟前',
+        title: '#小七与咩咩来约会',
+        content: 'adsadasdfasdfasdfasdfasdfasdf',
+        imgs: ['./img/menu12.png', './img/menu2.png'],
+        comments: '23',
+        like: '12'
+      },
+
+    ],
   },
   swiperChange: function (e) {
     this.setData({
@@ -55,7 +86,7 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-      console.log("58-------:"+this.data.test[0].info[0].name);
+    console.log("58-------:" + this.data.like_comments[0].imgs.length);
   },
 
   /**
@@ -109,7 +140,13 @@ Page({
 
   onItemClick: function (event){
     wx.showToast({
-      title: event.target.dataset.postid + "",  //其中注意postId 的参数传递方式
+      title: event.target.dataset.name + "",  //其中注意postId 的参数传递方式
     })
+  },
+
+  goDetailArticle:function(event){
+  
+    console.log("151--------------:" + event.currentTarget.dataset.articleid);
   }
+
 })
