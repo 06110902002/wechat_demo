cbdcf69bfaf97c216f046d3375b0ee8afbf7f9e6
Liu Xiaobing <liuxiaobing@G114E1901117.local>
2019-08-10 16:43:56 +0800

    处理获取本地图片与底部输入框模块

--- pages/detail_article/detailArticle.js
+++ pages/detail_article/detailArticle.js
@@ -1,10 +1,19 @@
 // pages/detail_article/detailArticle.js
+var app = getApp();
 Page({
 
   /**
    * Page initial data
    */
   data: {
+    
+    statusBarHeight: 0,
+
+    height:0,
+    
+    isShowSelectPic:false,
+    selectImgPath:''
+    
 
   },
 
@@ -12,10 +21,41 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    // wx.setNavigationBarTitle({
+    //   title: '123',
+    // })
+  
+    this.configStatusBar();
+    this.loadArticleDetail(options.articleid);
     
-
+  },
+  back:function(){
+    wx.navigateBack();
   },
 
+  /**
+   * 配置状态栏属性
+   */
+  configStatusBar:function(){
+    var self = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        self.setData({
+          statusBarHeight: res.statusBarHeight
+        });
+      }
+    })
+  },
+
+  loadArticleDetail: function (articleid){
+    if(articleid){
+      
+    }else{
+      wx.showToast({
+        title: '文章不存在或者文章id异常',
+      })
+    }
+  },
   /**
    * Lifecycle function--Called when page is initially rendered
    */
@@ -63,5 +103,62 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+
+
+
+  bindfocus: function (e) {
+
+    let that = this;
+    let height = 0;
+    let height_02 = 0;
+
+    wx.getSystemInfo({
+      success: function (res) {
+        height_02 = res.windowHeight;
+      }
+
+    })
+
+    height = e.detail.height - (app.globalData.height_01 - height_02);
+
+    console.log('app is', app.globalData.height_01);
+
+    that.setData({
+      height: height,
+    })
+    console.log('获得焦点的 e is', e);
+
+  },
+
+  //监听input失去焦点
+
+  bindblur: function (e) {
+
+    this.setData({
+      height: 0,
+    });
+    console.log('失去焦点的 e is', e);
+
+  },
+
+  selecImg:function(){
+    this.setData({
+      isShowSelectPic:!this.data.isShowSelectPic
+    });
+    var self = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        self.setData({
+          selectImgPath:tempFilePaths
+        });
+      }
+    })
+  },
 })
--- pages/main/main.js
+++ pages/main/main.js
@@ -48,6 +48,7 @@ Page({
     //点赞评论数组
     like_comments:[
       {
+        articlId:'001',
         headImg:'./img/menu1.png',nick:'韦小宝',lasttime:'20分钟前',
         title:'#小七与咩咩来约会',
         content:'adsadasdfasdfasdfasdfasdfasdf',
@@ -57,6 +58,7 @@ Page({
       },
 
       {
+        articlId: '002',
         headImg: './img/menu6.png', nick: '乔峰', lasttime: '20分钟前',
         title: '#小七与咩咩来约会',
         content: '马路上此起彼伏的车鸣声，夹杂着凄凄惨惨的雨滴，倒也洗去了城市街头的龌蹉与浊尘。我无力地将头靠在起雾的车窗一角，无神的眼 眸，望向远方，默默承受着会呼吸的伤和痛.在失去点点后，我曾一度伤                   心好几周，每当看见它熟悉的玩偶和那独有的小窝时，心就被揪似的疼。不知为何，我突然萌生出再也不想养狗的想法，是的，我很爱狗狗，但是，我更怕这种一辈子也无法抹去的伤痕',
@@ -66,6 +68,7 @@ Page({
       },
 
       {
+        articlId: '003',
         headImg: './img/menu7.png', nick: '刘胡来', lasttime: '20分钟前',
         title: '#小七与咩咩来约会',
         content: 'adsadasdfasdfasdfasdfasdfasdf',
@@ -147,6 +150,9 @@ Page({
   goDetailArticle:function(event){
   
     console.log("151--------------:" + event.currentTarget.dataset.articleid);
+    wx.navigateTo({
+      url: '../detail_article/detailArticle?articleid=' + event.currentTarget.dataset.articleid
+    })
   }
 
 })
